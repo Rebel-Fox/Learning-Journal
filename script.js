ef3c03cebaf24b5ee60a2139d5a0b557c5a3e292
Rebel-Fox <rookiewebdev@outlook.com>
2024-06-17 11:08:35 +0530

    linked pages

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
-const homePage=document.getElementById("home-page")
-const featurePost=document.getElementById("feature-post")
-const aboutMe=document.getElementById("about-me")
-const recentPosts=document.getElementById("recent-posts")
-const homeBtn=document.getElementById("home-btn")
-const aboutMeBtn = document.getElementById("about-me-btn")
+
 const viewMoreContents = document.querySelectorAll(".view-more-content")
 const viewMore = document.getElementById("view-more")
-
-aboutMeBtn.addEventListener("click",function(){
-    homePage.classList.add("hidden")
-    recentPosts.classList.remove("hidden")
-    aboutMe.classList.remove("hidden")
-    featurePost.classList.add("hidden")
-})
-
-homeBtn.addEventListener("click",function(){
-    homePage.classList.remove("hidden")
-    recentPosts.classList.add("hidden")
-    aboutMe.classList.add("hidden")
-    featurePost.classList.add("hidden")
-})
-
-homePage.addEventListener("click",function(){
-    homePage.classList.add("hidden")
-    recentPosts.classList.remove("hidden")
-    featurePost.classList.remove("hidden")
-})
 
 viewMore.addEventListener("click",function(e){
     e.preventDefault()
